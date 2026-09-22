a346ab6113d92c8dd4760bca81d631736cfa857d
Kevito11 <kevito.valdez@gmail.com>
2026-09-22 09:58:53 -0400

    fix(deploy): restore base path to root for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/conferencia_despierta_upperroon/',
+  base: '/',
   plugins: [react()],
 })
